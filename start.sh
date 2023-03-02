8684432ce4b3e35fcba161d0c0642d20ffdf0aed
jayu2980 <124619960+jayu2980@users.noreply.github.com>
2023-03-03 00:03:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
+git clone https://github.com/jayu2980
+/
+jb_moviesearchbot/MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
